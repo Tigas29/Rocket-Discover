93d3ca31805eb869c5270e1b58e10615195541c7
tiago santos <tiagoalmeidasantos04@gmail.com>
2022-10-27 11:37:23 -0300

    js releases exercise finished

--- JS/lamp/script.js
+++ JS/lamp/script.js
@@ -1,48 +1,42 @@
 const btn = document.getElementById("btn");
-const acender = document.getElementById("acender");
-const apagar = document.getElementById("apagar");
-const zerar = document.getElementById("zerar");
-let box = document.getElementById("box");
-let img = document.createElement("img");
-let message = document.getElementById("message");
+let lamp = document.getElementById("lamp");
+const message = document.getElementById("message");
+lamp.addEventListener("mouseover", TurnOn);
+lamp.addEventListener("mouseleave", TurnOff);
+lamp.addEventListener("dblclick", LampBronken);
 
-box.addEventListener("mouseover", TurnOn);
-box.addEventListener("mouseleave", TurnOff);
-box.addEventListener("dblclick", LampBronken);
-acender.addEventListener("click", TurnOn);
+btn.addEventListener("click", Default);
 
-apagar.addEventListener("click", TurnOff);
-
-zerar.addEventListener("click", Default);
-
-function LampBronken() {
-  img.src =
-    "https://img.freepik.com/vetores-gratis/lampada-quebrada-realista_52683-74256.jpg?w=996&t=st=1666703888~exp=1666704488~hmac=f1d6358c231a7a2db3f31b520e3a51ba5ee1ba7744a8540822850348ceca44b6";
-
-  box.appendChild(img);
+function isBroken() {
+  // forma de verificar se há a imagem onde eu quero ver,
+  // se não tiver ele vai retorna um -1
+  return lamp.src.indexOf("quebrada") > -1;
 }
+function LampBronken() {
+  lamp.src = "./imgs/quebrada.png";
+}
+console.log(LampBronken);
 
 function Default() {
-  img.src = "";
-  box.appendChild(img);
+  if (lamp.src.indexOf("off") > -1) {
+    TurnOn();
+    btn.textContent = "Desligar";
+  } else {
+    TurnOff();
+    btn.textContent = "Ligar";
+  }
 }
 function TurnOn() {
-  if (
-    img.src !==
-    "https://img.freepik.com/vetores-gratis/lampada-quebrada-realista_52683-74256.jpg?w=996&t=st=1666703888~exp=1666704488~hmac=f1d6358c231a7a2db3f31b520e3a51ba5ee1ba7744a8540822850348ceca44b6"
-  ) {
-    img.src =
-      "https://img.freepik.com/vetores-gratis/lampada-realista-com-eletricidade_23-2149129410.jpg?w=826&t=st=1666659182~exp=1666659782~hmac=c4f5e1c930024dcc7cbf6ed56d85be873f750abe320d3a0a0a16349cbb47cff0";
-    box.appendChild(img);
+  if (!isBroken()) {
+    lamp.src = "./imgs/on.png";
+  } else {
+    message.textContent = "Reload the page to concert this lamp";
   }
 }
 function TurnOff() {
-  if (
-    img.src !==
-    "https://img.freepik.com/vetores-gratis/lampada-quebrada-realista_52683-74256.jpg?w=996&t=st=1666703888~exp=1666704488~hmac=f1d6358c231a7a2db3f31b520e3a51ba5ee1ba7744a8540822850348ceca44b6"
-  ) {
-    img.src =
-      "https://img.freepik.com/fotos-gratis/uma-lampada-nao-luminosa-paira-no-espaco-da-copia-de-fundo-desfocado-escuro_169016-9524.jpg?w=996&t=st=1666659277~exp=1666659877~hmac=0556734d23b16a1a0c2a2ef352999dcb5326ed955bc4b4b6050369f1fdd6859a";
-    box.appendChild(img);
+  if (!isBroken()) {
+    lamp.src = "./imgs/off.png";
+  } else {
+    message.textContent = "Reload the page to concert this lamp";
   }
 }
