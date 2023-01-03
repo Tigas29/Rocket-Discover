7aed36406b38ac19740b5272c3f26ace5d4516b0
Tiago Santos <tiagoalmeidasantos04@gmail.com>
2023-01-03 13:43:16 -0300

    Resolution exercise

--- JS/Modulo11Exercise/e2.js
+++ JS/Modulo11Exercise/e2.js
@@ -1,28 +1,28 @@
-let receitas = {
-  incomes: [10000, 10000, 2000],
-  expenser: [1000, 200, 210],
-};
+// let receitas = {
+//   incomes: [10000, 10000, 2000],
+//   expenser: [1000, 200, 210],
+// };
 
-function some(array) {
-  let total = 0;
-  for (value of array) {
-    total += value;
-  }
+// function some(array) {
+//   let total = 0;
+//   for (value of array) {
+//     total += value;
+//   }
 
-  return total;
-}
+//   return total;
+// }
 
-function calculate() {
-  const expenserTotal = some(receitas.expenser);
-  const insumesTotal = some(receitas.incomes);
+// function calculate() {
+//   const expenserTotal = some(receitas.expenser);
+//   const insumesTotal = some(receitas.incomes);
 
-  const total = expenserTotal - insumesTotal;
+//   const total = expenserTotal - insumesTotal;
 
-  if (total >= 0) {
-    alert("saudo positivo, parabens :" + total);
-  } else {
-    alert("saudo nnegativo, lamentavel :" + total);
-  }
-}
+//   if (total >= 0) {
+//     alert("saudo positivo, parabens :" + total);
+//   } else {
+//     alert("saudo nnegativo, lamentavel :" + total);
+//   }
+// }
 
-calculate();
+// calculate();
---
+++ JS/Modulo11Exercise/e4.js
@@ -0,0 +1,26 @@
+// let temperature = window.prompt(
+//   "Digite o valor da temperatura (ex: 50f / 50c n )"
+// );
+// let newTemperature = temperature.toLowerCase();
+
+// function ChangeToF() {
+//   let change = 0;
+//   let number = "";
+//   let result;
+//   if (newTemperature.includes("f") == true) {
+//     number = newTemperature.replace("f", "");
+//     change = ((number - 32) * 5) / 9;
+
+//     result = change + " graus em celsius";
+//   } else if (newTemperature.includes("c") == true) {
+//     number = newTemperature.replace("c", "");
+//     change = (number * 9) / 5 + 32;
+//     result = change + " graus em fahrenheit";
+//   } else {
+//     throw new Error("GRAU NAO IDENTIFICADO");
+//   }
+
+//   return alert(result);
+// }
+
+// ChangeToF();
